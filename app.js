ea5513fa8b2d2d75105fa0381b25ca13e5088fb6
Riddhi Patel <Ridshah2@gmail.com>
2023-02-09 14:18:50 -0500

    node-http

---
+++ app.js
@@ -0,0 +1,14 @@
+const express = require("express");
+
+const server = express();
+
+server.get("/", (req, res)=> {
+    res.write("<h1> Home: Riddhi's Pad</h1>");
+    res.status(200);
+    res.end();
+})
+
+
+server.listen(3000, () => {
+    console.log("Server started at localhost:3000");
+});
